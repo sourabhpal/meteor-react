56a55c75b5af5692cc1f53121d91eeab43dfcb4c
Sourabh Pal <sourabhp203@gmail.com>
2015-12-12 13:32:12 -0800

    App is finished and deployed at spal_todo.meteor.com test server

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -16,8 +16,14 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
+  // Only publish tasks that are public or belong to the current user
   Meteor.publish("tasks", function () {
-    return Tasks.find();
+    return Tasks.find({
+      $or: [
+        { private: {$ne: true} },
+        { owner: this.userId }
+      ]
+    });
   });
 }
 
@@ -37,10 +43,33 @@ Meteor.methods({
   },
  
   removeTask(taskId) {
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can delete it
+      throw new Meteor.Error("not-authorized");
+    }
+ 
     Tasks.remove(taskId);
   },
  
   setChecked(taskId, setChecked) {
+    const task = Tasks.findOne(taskId);
+    if (task.private && task.owner !== Meteor.userId()) {
+      // If the task is private, make sure only the owner can check it off
+      throw new Meteor.Error("not-authorized");
+    }
+ 
     Tasks.update(taskId, { $set: { checked: setChecked} });
-  }
+  },
+ 
+  setPrivate(taskId, setToPrivate) {
+    const task = Tasks.findOne(taskId);
+ 
+    // Make sure only the task owner can make a task private
+    if (task.owner !== Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Tasks.update(taskId, { $set: { private: setToPrivate } });
+}
 });
