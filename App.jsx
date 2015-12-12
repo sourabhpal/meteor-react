eb36c16c8bc90b9ba3ccf6bb4e2dd341944503ac
Sourabh Pal <sourabhp203@gmail.com>
2015-12-12 10:55:34 -0800

    Added CSS, as well as adding tasks from UI to Mongo, and hiding completed tasks

--- App.jsx
+++ App.jsx
@@ -1,24 +1,78 @@
 // App component - represents the whole app
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
+ 
+  // This mixin makes the getMeteorData method work
+  mixins: [ReactMeteorData],
+
+  getInitialState() {
+    return {
+      hideCompleted: false
+    }
+  },
+ 
+  // Loads items from the Tasks collection and puts them on this.data.tasks
+  getMeteorData() {
+    let query = {};
+ 
+    if (this.state.hideCompleted) {
+      // If hide completed is checked, filter tasks
+      query = {checked: {$ne: true}};
+    }
+ 
+    return {
+      tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch()
+    };
   },
  
   renderTasks() {
-    return this.getTasks().map((task) => {
+    // Get tasks from this.data.tasks
+    return this.data.tasks.map((task) => {
       return <Task key={task._id} task={task} />;
     });
   },
+  
+  handleSubmit(event) {
+    event.preventDefault();
  
+    // Find the text field via the React ref
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+ 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+ 
+    // Clear form
+    React.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: ! this.state.hideCompleted
+    });
+  },  
+  
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly={true}
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted} />
+            Hide Completed Tasks
+          </label>
+          
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
         </header>
  
         <ul>
