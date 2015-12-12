78092bf7196bae237187a8e924141ff08d40350a
Sourabh Pal <sourabhp203@gmail.com>
2015-12-12 11:31:23 -0800

    Removed autopublish and insecure. Connections to client, server, and database are now secure

--- Task.jsx
+++ Task.jsx
@@ -8,13 +8,11 @@ Task = React.createClass({
 
   toggleChecked() {
     // Set the checked property to the opposite of its current value
-    Tasks.update(this.props.task._id, {
-      $set: {checked: ! this.props.task.checked}
-    });
+    Meteor.call("setChecked", this.props.task._id, ! this.props.task.checked);
   },
  
   deleteThisTask() {
-    Tasks.remove(this.props.task._id);
+    Meteor.call("removeTask", this.props.task._id);
   },
 
   render() {
@@ -35,7 +33,7 @@ Task = React.createClass({
           onClick={this.toggleChecked} />
  
         <span className="text">
-          <strong>{this.props.task.username}</strong>: {this.props.task.text}
+          <strong>{this.props.task.username}</strong>{this.props.task.text}
         </span>
       </li>
     );
--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -6,9 +6,41 @@ if (Meteor.isClient) {
   Accounts.ui.config({
     passwordSignupFields: "USERNAME_ONLY"
   });
-   
+
+  Meteor.subscribe("tasks");
+
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
   });
 }
+
+if (Meteor.isServer) {
+  Meteor.publish("tasks", function () {
+    return Tasks.find();
+  });
+}
+
+Meteor.methods({
+  addTask(text) {
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error("not-authorized");
+    }
+ 
+    Tasks.insert({
+      text: text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username
+    });
+  },
+ 
+  removeTask(taskId) {
+    Tasks.remove(taskId);
+  },
+ 
+  setChecked(taskId, setChecked) {
+    Tasks.update(taskId, { $set: { checked: setChecked} });
+  }
+});
